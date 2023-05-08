b1a7c199d470bf1a34a88b09c73943fde19c28cd
Gingerty <101673472+Gingerty@users.noreply.github.com>
2023-05-07 20:55:32 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-
 
 public class Main {
 	
